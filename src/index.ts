0b6b1de4150a6947f77a0ab88c17b0a5a2b1c943
Hafizhun Soekiswo <hsoekiswo@gmail.com>
2024-12-19 19:33:32 +0800

    feat: initialized open api docs, implement .src/index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import { Hono } from 'hono';
+import { swaggerUI } from "@hono/swagger-ui";
+import { OpenAPIHono, createRoute } from "@hono/zod-openapi";
+import { z } from 'zod';
 import monsters from './monsters/index';
 import drops from './drops/index';
 import items from './items/index';
@@ -7,16 +9,40 @@ import experiences from './experiences/index';
 import { handleError } from './errorHandler';
 import summaries from './summaries/index';
 
-const app = new Hono();
+const app = new OpenAPIHono();
+app.doc("/doc", {
+  openapi: "3.0.0",
+  info: {
+    version: "1.0.0",
+    title: "Ragnarok API",
+  }
+})
 
 app.onError((error, c) => {
   const { status, body } = handleError(error, c);
   return c.json(body, status);
 });
 
-app.get('/', (c) => {
-  return c.text("Hello this is mini Ragnarok monsters database api for learning!")
-})
+const indexRoute = createRoute({
+  method: "get",
+  path: "/",
+  responses: {
+    200: {
+      content :{
+        'text/plain': {
+          schema: z.string(),
+        }
+      },
+      description: "Say hello to the users",
+    },
+  },
+});
+
+app.openapi(indexRoute, (c) => {
+  return c.text('Hello this is mini Ragnarok monsters database api. This API can provide items and levelling recommendation!', 200)
+});
+
+app.get('/ui', swaggerUI({ url: "/doc" }));
 
 app.route('/monsters', monsters);
 app.route('/drops', drops);
@@ -25,4 +51,4 @@ app.route('/maps', maps);
 app.route('/experiences', experiences);
 app.route('/summaries', summaries);
 
-export default app
+export default app;
