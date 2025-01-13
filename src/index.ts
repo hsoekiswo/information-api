c2fb13054f53f6a69682a6ac37946f2bf9b1c464
Hafizhun Soekiswo <hsoekiswo@gmail.com>
2025-01-13 17:47:09 +0700

    docs: update documentation

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ app.doc("/doc", {
   },
   tags: [
     { name: 'Main', description: 'Main endpoint' },
+    { name: 'Data', description: 'Endpoints for complete info of monsters'},
     { name: 'Characters', description: 'Endpoints for character to create, read, update and delete ragnarok character' },
     { name: 'Recommendations', description: 'Endpoints for user to get recommendation of item drops and leveling' },
     { name: 'Monsters', description: 'Endpoints related to monsters' },
